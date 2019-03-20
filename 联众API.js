56c9c25cea8ed364105f26759e343c0af4d2e568
kstd2016 <48593363+kstd2016@users.noreply.github.com>
2019-03-21 00:34:51 +0800

    Update 联众API.js

--- 联众API.js
+++ 联众API.js
@@ -1,20 +1,27 @@
-U2FsdGVkX19YThSEcrD7wJF2HNaPnyA1xsDrvMERwYMBT4XwmaCJaiMKPJ0WvEva
-7JVeDIGrGAPG2B5rIwaH0VBZwc2uwFZFemVF0b0YAMVrK0P1sQJ7kSXU+N4dNr+A
-wA2eRM4q+REfEkj5TPqIM6wH3PjWg/Gz2amFArAKimFzRPlsv6/doHpPcvCoIE0W
-rfdHHjJaUrsSmLXcWr5pTPUr6ffUxx4/3ZxNVAzMv4B7PWtV4C5jhmc4FF9f+vPL
-hrSyOW/wP1ROD6xOwosuyuEr9pKVzL6Dcxwnv7Lv4TwdmRI+U4SNby1kew4uFTT1
-bDV62tyDw2zKHHCekgqwNx3t83IFSBbBqoMask/zbq+rpTV/pHUCcdCshAGsMldQ
-ni9yWy74ZbNGTDtjhknnHZfIOdAIjYlaJ33Z46GDHgtCbdK3F8PIq5NI0ik7dMZq
-sZdU5gJ6bxZtNNref2M+86c0EN1eqx+/gI6hG8VSTfhPKA/gm6CCd3nWoH0VxTt5
-PO4tenlQzIW1d8PfYiI/IcJvubwBCr4lVCpBwswY/ut4PfX4mvSz8VIiWyNfrbdn
-xU3QVX7pNwVlH5Ed4Ac+cqkfK2hNi/zad9KGM0k5k8Tc9dstgbsOyaSDituaGKxZ
-KJY6QgYQ39quJfHnFoP8ayUVXoqUXqIgLt2DMpDeki9q8McDlPZj8gXflfNQNvVh
-vY9v2SeDPQGnOXOvZsVTLQ9HVgZsqH1VFMw5SEBijbRLQ5Evo1UBDkzSeJ7th4ON
-n7Fhe4sLK93K5KYTHtThM6B4C+Eag5zv961KL2OaFvLURNxKX637E2UCsirYw+Vi
-OEkwhLAlDOakT1Z9MKfJayHy/oxbuK6KYL/zkmOWfpj3JGJD92uiD4zDYqxBcu6J
-wSgJgrK+MUqGCPXXEXXIRGw7XeQj67ovKc/BBCyqajoCFqLKuXVub71SLU+7n49U
-jxkXMedXSZOAyf3gSop3zTgmaEvXOOc5OspZPsBFmCjWNDcUibApnZVEgEZGqxlq
-0JjTobNiiQr0rONmxyjf26AFI6eYrPY7ry0RCbXWCBh0Vga2z/fc6i0HHLKR6Ds1
-k0WvgHhZhAu3xaImMj4bm6xniqoWGDJRx5eO0YE+m3+TYtqGR77J0g8nEasVh0RH
-BaadxgqiU0ToTCkqRzTR3In/EpYX1MSDLqGbY9RVMseBaMqjHzqF7NNkVq3I+AAE
-R+yHVu7zb4hm5XWoFhoPzTCDVXvib2aarOmlE889SJPZ53L+iR6wDIeYtSB+0jEn
+let 联众打码API = function(用户,密码){
+    this.打码 = function(a,b,c,d,图像类型ID){
+        base64 = this.getImgBase64(a,b,c,d)
+        var urls = http.postJson("https://v2-api.jsdama.com/upload", {
+            softwareId: "13153",  //软件ID
+            softwareSecret: "oAQ8tQRwJTcp6uog0oBxIXvX4DpIJuaZykPQgdoh",  //软件密钥
+            username: 用户,
+            password: 密码,
+            captchaData: base64,
+            captchaType: 图像类型ID,
+            captchaMinLength: 1,
+            captchaMaxLength: 1,
+        });
+        坐标 = urls.body.json().data.recognition.toString()
+        return 坐标
+        
+        /*else{
+            log(urls.body.json().message)
+            return "打码失败"
+        }*/
+    }
+    this.getImgBase64 = function(a,b,c,d){
+        clip = images.clip(captureScreen(),a,b,c,d);
+        return images.toBase64(clip);
+    } 
+}
+module.exports = 联众打码API
