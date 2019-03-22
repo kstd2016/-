8a1acf504d1c31589a429954182e3fd7c6cbb293
kstd2016 <48593363+kstd2016@users.noreply.github.com>
2019-03-23 00:48:02 +0800

    Update 联众API.js

--- 联众API.js
+++ 联众API.js
@@ -1,5 +1,5 @@
 let 联众打码API = function(用户,密码){
-    this.打码 = function(a,b,c,d,图像类型ID){
+    this.打码 = function(a,b,c,d,图像类型ID,MinLength,MaxLength){
         base64 = this.getImgBase64(a,b,c,d)
         var urls = http.postJson("https://v2-api.jsdama.com/upload", {
             softwareId: "13153",  //软件ID
@@ -8,8 +8,8 @@ let 联众打码API = function(用户,密码){
             password: 密码,
             captchaData: base64,
             captchaType: 图像类型ID,
-            captchaMinLength: 1,
-            captchaMaxLength: 1,
+            captchaMinLength: MinLength,
+            captchaMaxLength: MaxLength,
         });
         坐标 = urls.body.json().data.recognition.toString()
         return 坐标
