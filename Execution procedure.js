cf2dfccb5d52708fa7da55904cbc0793d65bb0dd
kstd2016 <48593363+kstd2016@users.noreply.github.com>
2019-03-30 01:26:48 +0800

    Update Execution procedure.js

--- Execution procedure.js
+++ Execution procedure.js
@@ -82,8 +82,8 @@ function 全局变量(){
     联众密码 = 同步参数[6];
 }
 function 超级点击(变量){
-        if(desc(变量).exists()==true){封装API.desc(变量).click()}
-        else if(text(变量).exists()==true){封装API.text(变量).click()}
+        if(descContains(变量).exists()==true){封装API.desc(变量).click()}
+        else if(textContains(变量).exists()==true){封装API.text(变量).click()}
 }
 function 进入叔叔不约(){
     获取号码参数 = 0
@@ -101,8 +101,12 @@ function 进入叔叔不约(){
             break;
         }
         if(id("a06").exists()==true){
-            封装API.id("a06").click();
-            break;
+            if(text("窗口").exists()==true){}
+            else{
+                返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+                封装API.setView(返回浏览器主页).click()
+                break;
+            }
         }
         if(text("立即提速").exists()==true){
             封装API.id("alh").click();
@@ -114,21 +118,30 @@ function 进入叔叔不约(){
         封装API.text("允许").click();
     }
     封装API.等待(500,1000)
+    
     if(text("允许").exists()==true){
         封装API.text("允许").click();
     }
-    封装API.id("a06").click();
+    if(text("窗口").exists()==true){}
+    else{
+        返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+        封装API.setView(返回浏览器主页).click()
+    }
     封装API.id("aks").click()
     封装API.id("lh").click()
+    封装API.id("lk").click()
     if(text("立即提速").exists()==true){
-        封装API.id("alh").click()
+        log("找到立即提速...")
+        关闭提速框 = classNameContains("Button").clickable(true).idContains("al").findOne(2000)
+        封装API.setView(关闭提速框).click()
     }
     封装API.等待(1000,2000)
     setText("http://www.unclenoway.com/#")
     封装API.text("前往").click()
     for(let a=0;a<10;a++){
         if(text("听花开的树洞服务号").exists()==true||desc("听花开的树洞服务号").exists()==true){
-            封装API.id("a06").click();
+            返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+            封装API.setView(返回浏览器主页).click()
             封装API.等待(1000,2000)
             进入叔叔不约()
             break;
@@ -154,12 +167,16 @@ function 进入叔叔不约(){
         if(desc("拖动下方滑块完成拼图").exists()==true||text("拖动下方滑块完成拼图").exists()==true){
             接码模块()
         }
+        else if(textContains("请点击").exists()==true&&idContains("reload").exists()==true){
+            接码模块()
+            }
         else{超级点击("点击完成验证")}
     }
     for(let a=0;a<6;a++){
         sleep(5000)
         if(text("听花开的树洞服务号").exists()==true||desc("听花开的树洞服务号").exists()==true){
-            封装API.id("a06").click();
+            返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+            封装API.setView(返回浏览器主页).click()
             封装API.等待(1000,2000)
             进入叔叔不约()
             break;
@@ -188,6 +205,9 @@ function 进入叔叔不约(){
         if(desc("拖动下方滑块完成拼图").exists()==true||text("拖动下方滑块完成拼图").exists()==true){
             接码模块()
         }
+        else if(textContains("请点击").exists()==true&&idContains("reload").exists()==true){
+            接码模块()
+            }
         else{超级点击("点击完成验证")}
     }
     for(let a=0;a<10;a++){
@@ -211,6 +231,9 @@ function 进入叔叔不约(){
         if(desc("拖动下方滑块完成拼图").exists()==true||text("拖动下方滑块完成拼图").exists()==true){
             接码模块()
         }
+        else if(textContains("请点击").exists()==true&&idContains("reload").exists()==true){
+            接码模块()
+            }
         else{超级点击("点击完成验证")}
     }
     //textContains("输入信息").exists()==true不可用
@@ -229,7 +252,8 @@ function 进入叔叔不约(){
             }
             else if(获取验证码=="3001"){
                 toastLog("获取验证码3001")
-                封装API.id("a06").click();
+                返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+                封装API.setView(返回浏览器主页).click()
                 封装API.等待(1000,2000)
                 进入叔叔不约()
             }
@@ -260,7 +284,8 @@ function 开始聊天(){
         }
         if(text("窗口").id("bku").exists()==true){
             log("找到浏览器窗口控件...");
-            封装API.id("a06").click();
+            返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+            封装API.setView(返回浏览器主页).click()
             封装API.等待(1000,2000)
             进入叔叔不约()
             break;
@@ -271,7 +296,8 @@ function 开始聊天(){
         }
         if(textContains("由于实名制要求").exists()==true||descContains("由于实名制要求").exists()==true){
             log("找到由于实名制要求...")
-            封装API.id("a06").click();
+            返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+            封装API.setView(返回浏览器主页).click()
             封装API.等待(1000,2000)
             进入叔叔不约()
         }
@@ -284,7 +310,8 @@ function 开始聊天(){
                 请稍等参数++
                 if(请稍等参数>=10){
                     log("长时间在请稍等页面...").
-                    封装API.id("a06").click();
+                    返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+                    封装API.setView(返回浏览器主页).click()
                     封装API.等待(1000,2000)
                     进入叔叔不约()
                     break;
@@ -292,7 +319,12 @@ function 开始聊天(){
             }
             else{sleep(20)}
         }
-        if(desc("拖动下方滑块完成拼图").exists()==true||text("拖动下方滑块完成拼图").exists()==true){接码模块()}
+        if(desc("拖动下方滑块完成拼图").exists()==true||text("拖动下方滑块完成拼图").exists()==true){
+            接码模块()
+        }
+        else if(textContains("请点击").exists()==true&&idContains("reload").exists()==true){
+            接码模块()
+        }
         else{超级点击("点击完成验证")}
         if(聊天判断 == "对方离开了"){
             超级点击("离开")
@@ -349,6 +381,9 @@ function 固定时间回复(变量){
         if(desc("拖动下方滑块完成拼图").exists()==true||text("拖动下方滑块完成拼图").exists()==true){
             接码模块()
         }
+        else if(textContains("请点击").exists()==true&&idContains("reload").exists()==true){
+            接码模块()
+        }
         else{超级点击("点击完成验证")}
         /*if(text("开始聊天").exists()==true||desc("开始聊天").exists()==true){
             封装API.等待(1000,2000)
@@ -359,7 +394,8 @@ function 固定时间回复(变量){
             超级点击("开始聊天")
         }*/
         if(text("听花开的树洞服务号").exists()==true||desc("听花开的树洞服务号").exists()==true){
-            封装API.id("a06").click();
+            返回浏览器主页 = className("TextView").boundsInside(598,1724,914,1920).findOne(2000)
+            封装API.setView(返回浏览器主页).click()
             封装API.等待(1000,2000)
             进入叔叔不约()
         }
@@ -422,6 +458,10 @@ function 固定时间回复(变量){
                 }
             });
             消息 = 聊天内容[c-1]
+            回复消息 = "无关键词回复"//关键词回复模块();
+        }
+        else{
+            回复消息 = "无关键词回复"//关键词回复模块();
         }
         log("获取的消息为===>"+消息)//最后一条消息内容
         if(消息=="请进行验证~"){
@@ -433,161 +473,108 @@ function 固定时间回复(变量){
         if(账号参数 == "账号已死"){
             break;
         }
-        if(时间变量==回复等待时长-1){
-            setText(套路回复[变量][random(0,套路回复[变量].length-1)])
+        if(回复消息 == "无关键词回复"){
+            if(时间变量==回复等待时长-1){
+                setText(套路回复[变量][random(0,套路回复[变量].length-1)])
+                sleep(500)
+                超级点击("发送")
+                超级点击("留步")
+            }
+        }
+        else{
+            setText(回复消息)
             sleep(500)
             超级点击("发送")
             超级点击("留步")
+            return
         }
 
     } 
 }
-/*function 套路回复功能A(变量){
-    for(let a=0;a<回复等待时长;a++){
-        sleep(2000)
-        log("A等待时长===>"+a)
-        找到消息变量=0
-        if(textContains("不可以开多个浏览器").exists()==true||descContains("不可以开多个浏览器").exists()==true){
-            超级点击("确定")
-        }
-        if(text("确定离开?").exists()==true||desc("确定离开?").exists()==true){
-            超级点击("取消")
-        }
-        if(text("分享设置").exists()==true||desc("分享设置").exists()==true){
-            超级点击("关闭")
-            }
-        c = 0
-        聊天 = className("android.view.View").clickable(false).depth(19).find()
-        聊天.forEach(element => {
-            if(element.text()!=null&element.text()!=""){
-                聊天内容[c] = element.text()
-                c++
-                if(element.text()=="对方离开了。"){
-                    log("对方离开了...")
-                    聊天判断 = "对方离开了"
-
+function 关键词回复模块(){
+    关键词回复变量 = -1
+    引流微信参数 = -1
+    关键词=[];
+    随机回复=[];
+    关键词[0] = Array("你好","嗨","哈喽","hello","hi","嗨喽","小姐姐好","你好小姐姐","你好美女","美女好","美女你好")
+    关键词[1] = Array("聊污","聊骚","聊色")
+    关键词[2] = Array("请你吃饭", "一起吃", "看电影","一起玩","出来吗","一起去玩","出来玩","带你出去玩","见个面","见面")
+    关键词[3] = Array("在干吗", "在干嘛", "在做什么","干嘛呢","干吗呢","在干啥")
+    关键词[4] = Array("约一个", "约吗","约不","约个","约出来","约?")
+    关键词[5] = Array("QQ","加Q","加扣","Q友","扣扣")
+    关键词[6] = Array("人呢","在不在","在不","在吗")
+    关键词[7] = Array("不说话")
+    关键词[8] = Array("有男朋友", "男票","有对象")
+    关键词[9] = Array("微信", "加你", "联系", "给我", "留个", "加个微信", "vx","wx","加V","加个V","加Ｖ","加个Ｖ")
+    关键词[10] = Array("哪里人", "哪儿的","本地人")
+    关键词[11] = Array("住这里","住在这","住这")
+    关键词[12] = Array("加你了")
+    关键词[13] = Array("玩啊")
+    关键词[14] = Array("怎么称呼", "你叫什么")
+    关键词[15] = Array("住哪个小区", "住哪里", "住哪","什么地方","在哪里的")
+    关键词[16] = Array("吃了没", "吃没", "吃晚饭了", "吃午饭了", "吃中饭了", "吃饭了")
+    关键词[17] = Array("哪里玩", "在哪玩")
+    关键词[18] = Array("什么电视")
+    关键词[19] = Array("很近","好近","有点近")
+    关键词[20] = Array("什么游戏","玩啥游戏")
+    关键词[21] = Array("不会说话")
+    关键词[22] = Array("多高")
+    关键词[23] = Array("怎么啦")
+    关键词[24] = Array("在吗")
+    关键词[25] = Array("[微笑]","[大笑]")
+    关键词[26] = Array("节快乐","节日快乐")
+    关键词[27] = Array("游戏直播？","游戏直播吗？")
+    关键词[28] = Array("在么","在吗")
+    关键词[29] = Array("怎么了")
+    随机回复[0] = Array("你好呀", "你好", "你好","你好呀,很高兴认识你")
+    随机回复[1] = Array("好啊,要不Ｖ聊把,方便发图", "可以啊,要不微聊吧,方便语音发图")
+    随机回复[2] = Array("好啊，那我们Ｖ聊吧"+"!", "那你加我聊吧方便点，探探用的少"+"!", "那Ｖ聊吧方便点，"+"!")
+    随机回复[3] = Array("我在看电视", "我在看抖音呀", "我在刷抖音")
+    随机回复[4] = Array("那我们Ｖ聊吧方便点，"+"!", "加我Ｖ聊吧方便点，"+"!", "那加我Ｖ聊吧方便点，"+"!")
+    随机回复[5] = Array("我很少用那个，我都是用微信的"+"!", "我不用QQ的 要不加个V吧 "+"!", "没有用只有微信"+"!")
+    随机回复[6] = Array("在呢,这个用的的比较少，要不加我微信聊吧"+"!","在呢 怎么了","加我Ｖ信聊吧"+"!"+"探探用得少")
+    随机回复[7] = Array("我这里上的少 你加我Ｖ聊吧"+"!"+"","我探探用的不太多啦，要不你加我微信把"+"!", "探探我就偶尔上啦")
+    随机回复[8] = Array("没有呢，你呢", "暂时没有呢，你呢", "目前没有，你呢")
+    随机回复[9] = Array("恩，"+"!", "可以,"+"!", "恩,那我留个Ｖ吧，"+"!")
+    随机回复[10] = Array("我是浙江嘉兴的", "浙江嘉兴的呀", "浙江嘉兴这边的")
+    随机回复[11] = Array("我也是住这里", "我也住这里", "好吧，我也住这边")
+    随机回复[12] = Array("好的，待会我去通过", "好的收到，待会我去通过", "恩，待会我去通过")
+    随机回复[13] = Array("你玩什么啊？", "你玩什么游戏？", "你玩什么的？")
+    随机回复[14] = Array("我叫婷婷", "婷婷", "我叫婷婷")
+    随机回复[15] = Array("不告诉你", "现在还不能告诉你，以后熟了可以", "熟悉了再告诉你")
+    随机回复[16] = Array("没呢，准备吃了", "准备吃饭了", "正准备吃呢")
+    随机回复[17] = Array("在家看电视呢", "在家玩游戏呢", "在家玩游戏呀")
+    随机回复[18] = Array("绿肥红瘦", "绿肥红瘦", "绿肥红瘦[花痴]")
+    随机回复[19] = Array("确实蛮近的", "是挺近的", "好像确实蛮近的[花痴]")
+    随机回复[20] = Array("王者荣耀或者吃鸡这种啊，我会玩玩王者", "我就会玩王者农药", "王者还有吃鸡这种啊，我会玩玩王者")
+    随机回复[21] = Array("好吧，那你是做什么的？", "好吧，那你是做什么工作的？", "好吧，那你是干什么的呀？")
+    随机回复[22] = Array("166，你呢", "166cm，你呢", "166，你呢多高")
+    随机回复[23] = Array("就随便问问，如果不方便说就算了","就随便问问，如果不方便说也没关系","随便问问，如果不方便说就算了")
+    随机回复[24] = Array("在呀,怎么了?")
+    随机回复[25] = Array("[微笑]","","[大笑]")
+    随机回复[26] = Array("同乐同乐","谢谢,","谢谢,同乐")
+    随机回复[27] = Array("游戏客服，o(╯□╰)o","游戏客服啦，o(╯□╰)o")
+    随机回复[28] = Array("在啊","在呀")
+    随机回复[29] = Array("没事,就随便聊下啊")
+    for(let a=0;a<关键词.length;a++){
+        sleep(10)
+        关键词[a].forEach(element => {
+            if(消息.indexOf(element) != -1 ){
+                回复 = 随机回复[a][random(0,随机回复[a].length-1)]
+                关键词回复变量 = 1
+                if(a==2||a==4||a==5||a==6||a==7||a==9){
+                    引流微信参数 = 1
                 }
-                if(desc("重新开始").exists()==true||text("重新开始").exists()==true){
-                    超级点击("重新开始")
-                }
-            }
+            } 
         });
-        消息 = 聊天内容[c-1]
-        if(消息==null){
-            c = 0
-            聊天 = className("android.view.View").clickable(true).depth(18).find()
-            聊天.forEach(element => {
-                if(element.text()!=null&element.desc()!=""){
-                    聊天内容[c] = element.desc()
-                    c++
-                    if(element.desc()=="对方离开了。"){
-                        log("对方离开了...")
-                        聊天判断 = "对方离开了"
-    
-                    }
-                    if(desc("重新开始").exists()==true||text("重新开始").exists()==true){
-                        超级点击("重新开始")
-                    }
-                }
-            });
-            消息 = 聊天内容[c-1]
-        }
-        log("获取的消息为===>"+消息)//最后一条消息内容
-        if(聊天判断 == "对方离开了"){
-            break;
-        }
-        if(消息!=null){
-            套路回复[变量].forEach(element => {
-                if(消息.indexOf(element) != -1 ){
-                       找到消息变量 = 1
-                       log("找到自己的消息"+a)//找到了自己的消息
-                } 
-            });
-        }
-        if(找到消息变量 != 1){
-            setText(套路回复[变量][random(0,套路回复[变量].length-1)])
-            log("A===>"+套路回复[变量][random(0,套路回复[变量].length-1)])
-            sleep(400)
-            超级点击("发送")
-            超级点击("留步")
-            回复变量 = 1
-            break;
-        }
+    } 
+    if(关键词回复变量 == 1){
+        return "无关键词回复"
+    }
+    else{
+        return "无关键词回复"
     } 
 }
-function 套路回复功能B(变量){
-    for(let a=0;a<回复等待时长;a++){
-        sleep(2000)
-        log("B等待时长===>"+a)
-        找到消息变量=0
-        if(textContains("不可以开多个浏览器").exists()==true||descContains("不可以开多个浏览器").exists()==true){
-            超级点击("确定")
-        }
-        if(text("确定离开?").exists()==true||desc("确定离开?").exists()==true){
-            超级点击("取消")
-        }
-        if(text("分享设置").exists()==true||desc("分享设置").exists()==true){
-            超级点击("关闭")
-            }
-        c = 0
-        聊天 = className("android.view.View").clickable(false).depth(19).find()
-        聊天.forEach(element => {
-            if(element.text()!=null&element.text()!=""){
-                聊天内容[c] = element.text()
-                c++
-                if(element.text()=="对方离开了。"){
-                    log("对方离开了...")
-                    聊天判断 = "对方离开了"
-
-                }
-                if(text("重新开始").exists()==true){
-                    超级点击("重新开始")
-                }
-            }
-        });
-        消息 = 聊天内容[c-1]//最后一条消息内容
-        if(消息==null){
-            c = 0
-            聊天 = className("android.view.View").clickable(true).depth(18).find()
-            聊天.forEach(element => {
-                if(element.text()!=null&element.desc()!=""){
-                    聊天内容[c] = element.desc()
-                    c++
-                    if(element.desc()=="对方离开了。"){
-                        log("对方离开了...")
-                        聊天判断 = "对方离开了"
-    
-                    }
-                    if(desc("重新开始").exists()==true||text("重新开始").exists()==true){
-                        超级点击("重新开始")
-                    }
-                }
-            });
-            消息 = 聊天内容[c-1]
-        }
-        log("获取的消息为===>"+消息)//最后一条消息内容
-        if(聊天判断 == "对方离开了"){
-            break;
-        }
-        if(消息!=null){
-            套路回复[变量].forEach(element => {
-                if(消息.indexOf(element) != -1 ){
-                       找到消息变量 = 1
-                       log("找到自己的消息"+a)//找到了自己的消息
-                } 
-            });
-        }
-        if(找到消息变量 != 1){
-            setText(套路回复[变量+1][random(0,套路回复[变量+1].length-1)])
-            log("B===>"+套路回复[变量+1][random(0,套路回复[变量+1].length-1)])
-            sleep(400)
-            超级点击("发送")
-            超级点击("留步")
-            回复变量 = 1
-            break;
-        }
-    } 
-}*/
 function 多开分身(){
     sleep(1000)
     launchApp("多开分身")
@@ -868,6 +855,29 @@ function 接码模块(){
                 }
             }
             else{封装API.idCon("reload").click()}
+        }
+        else if(textContains("请点击").exists()==true&&idContains("reload").exists()==true){
+            滑块 = classNameContains("android.view.View").boundsInside(21,486,1053,1518).idContains("tcaptcha_iframe").findOne(1000)
+            if(滑块!=null){
+                滑块图片 = 滑块.bounds()
+                console.log(滑块图片);
+                var X1 = 滑块图片.left
+                var Y1 = 滑块图片.top
+                var X2 = 滑块图片.right
+                var Y2 = 滑块图片.bottom
+                var 联众打码API = require('./联众API');
+                var 联众打码api = new 联众打码API(联众账号,联众密码);
+                坐标 = 联众打码api.打码(X1,Y1+35,X2-X1,Y2-Y1-253,"1303","1","1");
+                log(坐标)
+                拼图坐标 = 坐标.split("|")
+                文字点击坐标=[]
+                for(let a=0;a<拼图坐标.length;a++){
+                    sleep(1000)
+                    文字点击坐标[a] = 拼图坐标[a].split(",")
+                    封装API.press(X1+parseInt(文字点击坐标[a][0]),Y1+35+parseInt(文字点击坐标[a][1]),200)
+                }
+                sleep(1000)
+            }
         }
         else{break;}
     }
